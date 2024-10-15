dcfefc417b1dd9f77947228d644d06523b37e188
Pratik Atul Gaikwad <93657448+PratikG345@users.noreply.github.com>
2024-10-15 09:07:40 +0000

    hello

--- main.js
+++ main.js
@@ -1,21 +1,60 @@
+import React, { useState } from "react";
+import ReactDom from "react-dom/client"; 
 
- 
-// const head = React.createElement("h1", {id: "heading"}, "Hello React"); // we give attributes to a tag in {} eg :{id: "heading"}
-// console.log(head);
+const Header = () =>{
+    return(
+    <div className="header">
+        <div className="logo-container">
+          <img className="logo" src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png" />
+        </div>
+        <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+        </div>
+    </div>
+    )
+}
 
-// const paragraph = React.createElement("p", {}, "This is a paragraph in React")
+const RestuarantCard = () =>{
+    return(
+        <div className="res-card">
+            <img className="res-image" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/4/17/e2270129-d210-4a35-b044-73ae307c5280_235624.JPG" />
+            <h3>KFC</h3>
+            <div className="card1">
+                <p>4.1</p>
+                <p>25-30mins</p>
+            </div>
+            <p>Burgers, Fastfood, Rolls & Wraps</p>
+        </div>
 
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child"},[
-        React.createElement("h1",{key: "h1"},"This is h1 tag."),
-        React.createElement("h2",{key: "h2"},"This is h2 tag."),
-    ]),
-        React.createElement("div", {id: "child2"},
-            [React.createElement("h1",{key: "h1.2"},"This is h1 tag."),
-            React.createElement("h2",{key: "h2.2"},"This is h2 tag."),
-        ]),
-]);
+        
+    )
+}
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const Body =() =>{
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestuarantCard/>
+                <RestuarantCard/>
+            </div>
+        </div>
+    )
+}
 
-root.render(parent);
+const AppLayout = () =>{
+    return(
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<AppLayout />)

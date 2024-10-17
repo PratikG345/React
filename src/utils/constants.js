b33bf9ea9773c4526d9fb069f31ecedd651ecc9b
Pratik Atul Gaikwad <93657448+PratikG345@users.noreply.github.com>
2024-10-17 09:28:18 +0000

    utils

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
 export const LOGO_URL = "https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
-export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300/${cloudinaryImageId}"
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300/"
 
 // Example of Named Exports

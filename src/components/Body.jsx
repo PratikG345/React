0c0a828866926061eacf922fb40ee84c69cf4d42
Pratik <mrpratikonline@gmail.com>
2024-11-15 14:16:00 +0530

    body.jsx

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,47 @@
 import RestaurantCard from "./RestaurantCard";
-import resobj from "../utils/mockData";
+import { useState } from "react";
+import resObj from "../utils/mockData"
+// import resobj from "../utils/mockData";
 const Body = () => {
+    const [listOfRestaurants,setlistOfRestaurants] = useState(resObj);
+// it is array destructuring
+// it is same as 
+// const[listOfRestaurants,setListOfRestaurants] = arr
+// or
+// const listOfRestaurants = arr[0]
+// const setlistOfRestaurants = arr[1]
     return (
         <div className="body">
-            <div className="search">search</div>
+            <div className="filter">
+                <button 
+                className="filter-btn"
+                onClick={()=>{
+                    let filteredList = listOfRestaurants.filter(
+                        (resData) => resData.info.avgRating >= 4.3 
+                     );
+                     setlistOfRestaurants(filteredList)
+                     
+                }}
+                >
+                    Top Rated Restaurants
+                    </button>
+
+                    <button 
+                className="filter-price"
+                onClick={()=>{
+                    let filteredList = listOfRestaurants.filter(
+                        (resData) => resData.info.sla.deliveryTime <= 25 
+                     );
+                     setlistOfRestaurants(filteredList)
+                     
+                }}
+                >
+                    Under 25 minutes
+                    </button>
+            </div>
             <div className="res-container">
                 {
-                    resobj.map((restaurant) => (
+                    listOfRestaurants.map((restaurant) => (
                         <RestaurantCard key={restaurant.info.id} resData={restaurant} />
                     ))
                 }

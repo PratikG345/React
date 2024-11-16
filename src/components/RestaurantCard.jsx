75442339e48e4fdf5a3008eeef090bc9d39d8863
Pratik <mrpratikonline@gmail.com>
2024-11-16 19:22:05 +0530

    rescard

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -17,7 +17,7 @@ const RestaurantCard = (props) =>{
         <h4>{cuisines.join(", ")}</h4>
         <h4>{deliveryTime} minutes</h4>
         <h4>{avgRating}stars</h4>
-        <h4>{costForTwo}</h4>
+        <h4>Rs.{costForTwo} for two</h4>
         <h5>{locality}</h5>
   
     </div>
